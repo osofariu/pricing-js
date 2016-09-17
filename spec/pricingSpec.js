6392f984850cb75ceeaa6cbb79968b2716b961b7
osofariu <ovi@sofariu.com>
2016-09-17 12:22:28 -0400

    cleaned-up initial tests to make them more clear

--- spec/pricingSpec.js
+++ spec/pricingSpec.js
@@ -1,13 +1,13 @@
 require('jasmine')
 
-describe("Given a shopping list", function () {
+describe("Given a shopping list of items, each having a different price", function () {
 
     let list = require("../app/shopping-cart.js")
     let shoppingList
 
-    describe("And there's no applicable tax for any item in the cart", function () {
+    describe("And there's no applicable tax for any item in the cart (it's Food)", function () {
 
-        describe("and the price is not discounted", function () {
+        describe("and the total price of all items is less than $100", function () {
             let item = require("../app/item.js")
 
             it("the cost is the price of the one item in the list", function () {
@@ -15,10 +15,11 @@ describe("Given a shopping list", function () {
                 expect(shoppingList.cost()).toBe(100)
             })
 
-            it("the cost is the sum of the price of all items in the list", function () {
-                shoppingList = new list(new item.Food(100), new item.Food(200))
-                expect(shoppingList.cost()).toBe(300)
+            it("the cost is the sum of the price of all (Food) items in the list", function () {
+                shoppingList = new list(new item.Food(100), new item.Food(9899))
+                expect(shoppingList.cost()).toBe(9999)
             })
+
         })
     })
 })
